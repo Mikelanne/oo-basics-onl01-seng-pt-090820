fd618e29d8bb7bdcdeb6617a9cb9eade9419b553
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-24 18:30:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,6 +8,41 @@ class Shoe
     @brand = brand
   end 
   
+  def brand
+    @brand
+  end
+  
+  def color=(color)
+    @color = color
+  end
+  
+  def color
+    @color
+  end 
+  
+  def size=(size)
+    @size = size
+  end
+  
+  def size
+    @size
+  end 
+  
+  def material=(material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end 
+  
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @color
+  end 
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
